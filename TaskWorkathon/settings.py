eff80a5b5cce2f039fe65b67744d029ebfae13c1
sivapriya8113 <68690248+sivapriya8113@users.noreply.github.com>
2021-12-14 12:47:05 +0530

    Update settings.py

--- TaskWorkathon/settings.py
+++ TaskWorkathon/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'TaskWorkathon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ProductDB',
+        'USER': 'postgres',
+        'PASSWORD': 'Tech@18oct',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
